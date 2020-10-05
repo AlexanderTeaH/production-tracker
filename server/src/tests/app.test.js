ee156fdaedb5980b390125b28878b7ef03d6e4d1
AlexanderTeaH <alexanderthomashaselton@gmail.com>
2020-10-05 06:38:40 +0100

    Removed unused constant

--- server/src/tests/app.test.js
+++ server/src/tests/app.test.js
@@ -258,7 +258,6 @@ describe("GET /sites/production", () => {
         }
 
         const response = await request.get("/sites/production");
-        const query    = await ProductionSite.find().exec();
 
         expect(response.statusCode).toBe(200);
         expect(response.body.message).toBe("Found documents");
