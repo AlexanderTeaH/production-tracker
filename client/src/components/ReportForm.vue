b2d3b4d3e0a2b472da651890153218dfb6680dfe
AlexanderTeaH <alexanderthomashaselton@gmail.com>
2020-10-13 18:06:55 +0100

    Updated report form

--- client/src/components/ReportForm.vue
+++ client/src/components/ReportForm.vue
@@ -1,11 +1,11 @@
 <template>
     <v-card class="ma-6">
         <v-card-title>
-            <h3>Add site report</h3>
+            <h3>Add report</h3>
         </v-card-title>
         <v-card-text>
             <v-form
-            ref="form"
+                ref="form"
                 v-model="isValid"
                 px-3
             >
@@ -16,39 +16,107 @@
                     fill-height
                 >
                     <v-row>
-                        <v-col md="12" align="center">
+                        <!--
+                        <v-col align="center">
                             <v-date-picker
                                 v-model="date"
                                 first-day-of-week="1"
                                 no-title
                             ></v-date-picker>
                         </v-col>
-                        <v-col md="12">
+                        -->
+                        <v-col>
                             <v-select
-                                :items="sites"
-                                v-model="site"
-                                label="Site"
-                                prepend-icon="place"
+                                :items="reportTypes"
+                                v-model="reportType"
+                                label="Report type"
                                 outlined
                                 required
-                                :rules="[v => !!v || 'Site is required']"
+                                :rules="[v => !!v || 'Report type is required']"
                             ></v-select>
-                            <v-text-field
-                                v-model="volume"
-                                label="Volume (m³)"
-                                prepend-icon="local_gas_station"
-                                outlined
-                                required
-                                :rules="[v => !!v || 'Volume is required', numberRule]"
-                            ></v-text-field>
-                            <v-text-field
-                                v-model="temperature"
-                                label="Temperature (°C)"
-                                prepend-icon="filter_drama"
-                                outlined
-                                required
-                                :rules="[v => !!v || 'Temperature is required', numberRule]"
-                            ></v-text-field>
+                            <v-btn-toggle
+                                v-model="reportSubtype"
+                                mandatory
+                                tile
+                                color="primary"
+                                group
+                            >
+                                <v-btn
+                                    value="oil"
+                                    block
+                                >
+                                    <span class="hidden-sm-and-down">
+                                        Oil
+                                    </span>
+                                    <v-icon right>
+                                        local_gas_station
+                                    </v-icon>
+                                </v-btn>
+                                <v-btn
+                                    value="water"
+                                    block
+                                >
+                                    <span class="hidden-sm-and-down">
+                                        Water
+                                    </span>
+                                    <v-icon right>
+                                        waves
+                                    </v-icon>
+                                </v-btn>
+                            </v-btn-toggle>
+                            <v-row class="mt-6">
+                                <v-col v-if="reportType == 'Production report'">
+                                    <v-select
+                                        :items="sites"
+                                        v-model="site"
+                                        label="Site"
+                                        prepend-icon="place"
+                                        outlined
+                                        required
+                                        :rules="[v => !!v || 'Site is required']"
+                                    ></v-select>
+                                    <v-text-field
+                                        v-model="level"
+                                        label="Level (m)"
+                                        prepend-icon="square_foot"
+                                        outlined
+                                        required
+                                        :rules="[v => !!v || 'Level is required', numberRule]"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        v-model="volume"
+                                        label="Volume (m³)"
+                                        prepend-icon="local_gas_station"
+                                        outlined
+                                        required
+                                        :rules="[v => !!v || 'Volume is required', numberRule]"
+                                    ></v-text-field>
+                                    <v-text-field v-if="reportSubtype == 'oil'"
+                                        v-model="temperature"
+                                        label="Temperature (°C)"
+                                        prepend-icon="filter_drama"
+                                        outlined
+                                        required
+                                        :rules="[v => !!v || 'Temperature is required', numberRule]"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        v-model="density"
+                                        label="Density (g/cm³)"
+                                        prepend-icon="bubble_chart"
+                                        outlined
+                                        required
+                                        :rules="[v => !!v || 'Density is required', numberRule]"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        v-model="weight"
+                                        label="Weight (tons)"
+                                        prepend-icon="fitness_center"
+                                        outlined
+                                        required
+                                        :rules="[v => !!v || 'Weight is required', numberRule]"
+                                    ></v-text-field>
+                                </v-col>
+                            </v-row>
                         </v-col>
                     </v-row>
                 </v-container>
@@ -72,14 +140,22 @@ import axios from "axios";
 export default {
     name: "ReportForm",
     data: () => ({
-        sites: [
-            "X-1", "X-2", "X-3"
+        reportTypes: [
+            "Production report"
         ],
+        reportType:    null,
+        reportSubtype: null,
+        isValid:       true,
+
+        sites:       null,
         date:        new Date().toISOString().split("T")[0],
         site:        null,
+        level:       null,
         volume:      null,
         temperature: null,
-        isValid:     true,
+        density:     null,
+        weight:      null,
+
         numberRule: (v) => {
             if (!isNaN(v)) {
                 return true;
@@ -88,21 +164,32 @@ export default {
             return "Volume must be a number";
         }
     }),
+    created() {
+        this.fetchData();
+    },
     methods: {
+        async fetchData() {
+            this.sites = (await axios.get("http://127.0.0.1/sites/production"))
+                .data.documents.map(site => site.name);
+        },
         async submit() {
             const response = await axios
-                .post("http://192.168.1.219:80/addSiteReport", {
-                    date:        this.date,
+                .post(`http://127.0.0.1/reports/production/${this.reportSubtype}`, {
                     site:        this.site,
+                    level:       this.level,
                     volume:      this.volume,
-                    temperature: this.temperature
+                    temperature: this.temperature,
+                    density:     this.density,
+                    weight:      this.weight
                 });
             
             if (response.status == 201) {
                 this.$refs.form.reset();
             }
 
-            console.log(response);
+            else {
+                console.log(`Unexpected response submitting form:\n${response}`);
+            }
         }
     }
 };
