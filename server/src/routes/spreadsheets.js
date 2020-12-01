252636241252cfc53309fe2e3fb649b5184a7b54
AlexanderTeaH <alexanderthomashaselton@gmail.com>
2020-12-01 09:42:56 +0000

    Added monthly report generation

--- server/src/routes/spreadsheets.js
+++ server/src/routes/spreadsheets.js
@@ -103,6 +103,149 @@ router.get("/daily", async (request, response) => {
     }
 });
 
+router.get("/monthly", async (request, response) => {
+    try {
+        const year  = request.body.year;
+        const query = await Promise.all([
+            models.sites.well
+                .find()
+                .sort({ name: 1 })
+                .exec(),
+            models.production.daily.oil
+                .where("date")
+                .gte(new Date(year - 1, 11, 21))
+                .lte(new Date(year, 11, 20))
+                .exec()
+        ]);
+        
+        const monthlyProduction = {};
+
+        for (const wellSite of query[0]) {
+            monthlyProduction[wellSite.name] = new Array(12).fill().map(() => ({
+                totalDailyVolume: 0,
+                totalDailyWeight: 0
+            }));
+        }
+
+        for (const dailyProductionReport of query[1]) {
+            const wellSite = dailyProductionReport.wellSite;
+            const date     = new Date(dailyProductionReport.date);
+            const month    = date.getDate() > 20 ? (date.getMonth() + 1) % 12 : date.getMonth();
+            
+            monthlyProduction[wellSite][month].totalDailyVolume +=
+                dailyProductionReport.totalDailyVolume;
+            
+            monthlyProduction[wellSite][month].totalDailyWeight +=
+                dailyProductionReport.totalDailyWeight;
+        }
+
+        const workbook  = new excelJS.Workbook();
+        const worksheet = workbook.addWorksheet(year);
+
+        worksheet.getCell("B1").value = `All well oil production per month for the year ${year}`;
+        worksheet.getCell("B3").value = "Volume (m³)";
+
+        worksheet.columns = [
+            { key: "wellSite",  width: 20},
+            { key: "January",   width: 20 },
+            { key: "February",  width: 20 },
+            { key: "March",     width: 20 },
+            { key: "April",     width: 20 },
+            { key: "May",       width: 20 },
+            { key: "June",      width: 20 },
+            { key: "July",      width: 20 },
+            { key: "August",    width: 20 },
+            { key: "September", width: 20 },
+            { key: "October",   width: 20 },
+            { key: "November",  width: 20 },
+            { key: "December",  width: 20 },
+            { key: "yearTotal", width: 20 }
+        ];
+
+        worksheet.addRow([
+            "",
+            "January-20",
+            "February-20",
+            "March-20",
+            "April-20",
+            "May-20",
+            "June-20",
+            "July-20",
+            "August-20",
+            "September-20",
+            "October-20",
+            "November-20",
+            "December-20",
+            "Year total"
+        ]);
+
+        let isFirstRow = true;
+
+        for (const wellSite of Object.keys(monthlyProduction)) {
+            const totalDailyVolume = monthlyProduction[wellSite]
+                .map(x => x.totalDailyVolume);
+
+            const row = worksheet.addRow([wellSite, ...totalDailyVolume]);
+
+            if (isFirstRow) {
+                row.getCell("yearTotal").value = {
+                    formula:   "SUM($B5:$M5)",
+                    shareType: "shared",
+                    ref:       `N5:N${Object.keys(monthlyProduction).length * 2 + 7}`
+                };
+
+                isFirstRow = false;
+            }
+
+            else {
+                row.getCell("yearTotal").value = { sharedFormula: "N5" };
+            }
+        }
+
+        worksheet.addRow();
+        worksheet.addRow(["", "Weight (ton)"]);
+        worksheet.addRow([
+            "",
+            "January-20",
+            "February-20",
+            "March-20",
+            "April-20",
+            "May-20",
+            "June-20",
+            "July-20",
+            "August-20",
+            "September-20",
+            "October-20",
+            "November-20",
+            "December-20",
+            "Year total"
+        ]);
+
+        for (const wellSite of Object.keys(monthlyProduction)) {
+            const totalDailyWeight = monthlyProduction[wellSite]
+                .map(x => x.totalDailyWeight);
+
+            worksheet
+                .addRow([wellSite, ...totalDailyWeight])
+                .getCell("yearTotal")
+                .value = { sharedFormula: "N5" };
+        }
+
+        response
+            .status(200)
+            .attachment(`Monthly report ${year}.xlsx`);
+
+        await workbook.xlsx.write(response);
+    }
+
+    catch (error) {
+        console.log(`Error occured in "GET /spreadsheets/daily": ${error}`);
+        response
+            .status(500)
+            .json({ message: "Internal server error" });
+    }
+});
+
 const linkReports = (wellSites, dailyOilReports, oilTransportReports, dailyWaterReports, waterInjectionReports) => {
     const wellSiteData = {};
 
